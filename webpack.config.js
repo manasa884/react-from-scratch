fe86e3e5a8e0f6caa2223b4d546556ccff9d84c3
ManasaTipparam <manasa.tipparam@target.com>
2019-10-12 15:51:42 -0500

    completed tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require("webpack");
 
 
 module.exports = {
+  /* The point or points where to start the application bundling 
+   * process. 
+  */
   entry: "./src/index.js",
   mode: "development",
   module: {
@@ -15,19 +18,34 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        /* 'loader' is a shorthand for the 'use' property, when
+         * only one loader is being utilized. 
+         * (css-loader requires style-loader in order to work) 
+        */
         use: ["style-loader", "css-loader"]
       }
     ]
   },
+  /* Allows us to specify which extensions Webpack will resolve,
+   * allowing us to import modules without adding their extensions. 
+  */
   resolve: { extensions: ["*", ".js", ".jsx"] },
+  /* Where to place bundled code. */
   output: {
     path: path.resolve(__dirname, "dist/"),
+    /* Directory in which the bundle should go, and where 
+     * 'webpack-dev-server' should serve files from.
+    */
     publicPath: "/dist/",
     filename: "bundle.js"
   },
+  /* 'webpack-dev-server' set up */
   devServer: {
+    /* Location we're serving static files from */
     contentBase: path.join(__dirname, "public/"),
+    /* Port to run server */
     port: 3000,
+    /* Tells server where our bundled code is. */
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
   },
